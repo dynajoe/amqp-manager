7813cf525a7c2001a0b1a479047ef5b2d88dfeb6
Alex Munda <alex.munda3@gmail.com>
2018-04-20 15:03:19 -0500

    Add amqp types and config

--- src/amqp_manager.ts
+++ src/amqp_manager.ts
@@ -2,27 +2,46 @@ import { EventEmitter } from 'events'
 import { Logger } from './logger'
 import { AmqpConnectionFsm } from './connection_fsm'
 import * as _ from 'lodash'
+import * as Amqp from 'amqplib'
 
-export interface AmqpManagerConfig {
-   connection: {
-      protocol: string
-      user: string
-      password: string
-      host: string
-      port: number
-      vhost: string
-      params: {
-         heartbeat: number
-      }
-      options: null
+export interface AmqpExchangeConfig {
+   exchange: string
+   type: string
+   options: Amqp.Options.AssertExchange
+}
+
+export interface AmqpQueueConfig {
+   queue: string
+   options: Amqp.Options.AssertQueue
+}
+
+export interface AmqpBindingConfig {
+   exchange: string
+   queue: string
+}
+
+export interface AmqpConnectionConfig {
+   user: string
+   password: string
+   host: string
+   port: number
+   vhost: string
+   protocol?: string
+   params?: {
+      heartbeat: number
    }
-   queues: any[]
-   exchanges: any[]
-   bindings: any[]
+   options: null
+}
+
+export interface AmqpConfig {
+   connection: AmqpConnectionConfig
+   exchanges: AmqpExchangeConfig[]
+   queues: AmqpQueueConfig[]
+   bindings: AmqpBindingConfig[]
 }
 
 export class AmqpManager extends EventEmitter {
-   private config: AmqpManagerConfig
+   private config: AmqpConfig
    private fsm: any
    private started: boolean
    private closed: boolean
@@ -30,7 +49,7 @@ export class AmqpManager extends EventEmitter {
    private _channels: {}
    private _connection: null
 
-   constructor(config: AmqpManagerConfig) {
+   constructor(config: AmqpConfig) {
       super()
 
       this._channels = {}
@@ -88,11 +107,11 @@ export class AmqpManager extends EventEmitter {
       }
    }
 
-   async confirmChannel(name): Promise<any> {
+   async confirmChannel(name): Promise<Amqp.ConfirmChannel> {
       return this._channel('createConfirmChannel', name)
    }
 
-   async channel(name): Promise<any> {
+   async channel(name): Promise<Amqp.ConfirmChannel> {
       return this._channel('createChannel', name)
    }
 
